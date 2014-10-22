14cb1629ff75cc1eb9aa2dcf4a0cbe6aa6d92101
Salvador <salvadoredgar99@gmail.com>
2014-10-22 16:48:26 -0500

    Add List

--- app/src/main/java/amigos/com/conexionarduino/NegativeActivity.java
+++ app/src/main/java/amigos/com/conexionarduino/NegativeActivity.java
@@ -4,13 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
-public class NegativeActivity extends Activity {
+public class NegativeActivity extends Activity implements AdapterView.OnItemClickListener{
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_negative);
+        setContentView(R.layout.activity_dropset);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, getResources().getStringArray(R.array.excersises)));
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        listView.setItemChecked(position,true);
     }
 }
