f137aa55284aca8753b09bd8dde0f5d7c818cbcc
Salvador <salvadoredgar99@gmail.com>
2014-10-22 19:06:31 -0500

    Add seekBar Loaded Weight

--- app/src/main/java/amigos/com/conexionarduino/DropsetActivity.java
+++ app/src/main/java/amigos/com/conexionarduino/DropsetActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class DropsetActivity extends Activity implements AdapterView.OnItemClickListener {
+public class DropsetActivity extends Activity implements AdapterView.OnItemClickListener, SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
     private ListView listView;
+    private TextView textViewLoadedWeight;
+    private Button buttonStartEnd;
+    private boolean isStart;
+    private int positionItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +28,66 @@ public class DropsetActivity extends Activity implements AdapterView.OnItemClick
         listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, getResources().getStringArray(R.array.excersises)));
         listView.setOnItemClickListener(this);
+        positionItem = -1;
+
+        textViewLoadedWeight = (TextView) findViewById(R.id.textViewLoadedWeight);
+        textViewLoadedWeight.setText(getString(R.string.title_loaded_weight) + " 1");
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBarLoadedWeight);
+        seekBar.setOnSeekBarChangeListener(this);
+
+        buttonStartEnd = (Button) findViewById(R.id.buttonStartEnd);
+        buttonStartEnd.setOnClickListener(this);
+
+        isStart = false;
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
         listView.setItemChecked(position,true);
+        positionItem = position;
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.buttonStartEnd:
+
+                if (isStart) {
+                    buttonStartEnd.setText(R.string.btn_title_start);
+                }
+                else {
+                    if (positionItem!=-1){
+                        buttonStartEnd.setText(R.string.btn_title_exit);
+                        isStart = true;
+                    }
+                    else {
+                        Toast.makeText(this, R.string.select_excersise, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        textViewLoadedWeight.setText(getString(R.string.title_loaded_weight) + " " + (progress + 1));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+
 }
